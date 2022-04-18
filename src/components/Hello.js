e2a71dcc708dc1467e378fec2cb125c64949a872
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-19 00:42:03 +0500

    10, - ,Functional component

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,3 +1,5 @@
+// two function component bellow 
+// 1. javscript syntax
 // function Hello() {
 
 //     return <h1>Hello World Component!</h1>
@@ -5,7 +7,7 @@
 // }
 
 
-// ES6 syntax which provides the same functionality of the above javascript code
+// 2. ES6 syntax which provides the same functionality of the above javascript code
 const Hello = () => {
     return  <h1>Hello World Component- es6!</h1>
 }
