70c94a263bae57bc495fbf7213f4644788f565b2
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-19 00:51:16 +0500

    11, - ,Class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
+import Message from './components/Message';
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <Message/>
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,16 @@
+// Class Component 
+
+import React from 'react';
+// import Component from 'react';
+
+class Message extends React.Component 
+// class Message extends Component 
+{
+
+    render(){
+        return <h1>Hello from class component!</h1>
+    }
+
+}
+
+export default Message;
