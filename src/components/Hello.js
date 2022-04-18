dbba129a77fdaf7b9fcd8ac65ebd801d51f5f363
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-19 00:39:17 +0500

    9, - ,Component: ES6 syntax for creating component

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,14 @@
-function Hello() {
+// function Hello() {
 
-    return <h1>Hello World Component!</h1>
+//     return <h1>Hello World Component!</h1>
 
+// }
+
+
+// ES6 syntax which provides the same functionality of the above javascript code
+const Hello = () => {
+    return  <h1>Hello World Component- es6!</h1>
 }
+
 
 export default Hello;
