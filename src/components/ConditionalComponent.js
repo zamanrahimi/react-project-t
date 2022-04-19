f3df5438fe8dc8c4a8b0db1a2afbb3a19e0266af
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-20 01:14:27 +0500

    24, - ,Conditional Rendering using elment variable

--- src/components/ConditionalComponent.js
+++ src/components/ConditionalComponent.js
@@ -2,19 +2,20 @@ import {useState} from 'react';
 export default function ConditionalCOmponet(){
 
     const [display, setDisplay] = useState(true);
+    let output;
 
     if(display){
-        return(<div>
-            <h3>This is a ConditionalComponent</h3>
-            <button onClick={()=>setDisplay(false)}>Hide</button>
-        </div>);
-    
+        output = <h3>This is a ConditionalComponent</h3>
     }
     else{
-        return(<div>
-            <h3>There is nothing to be displayed</h3>
-            <button onClick={()=>setDisplay(true)}>Show</button>
-        </div>);
+        output = <h3>Nothing to show</h3>
     }
+
+    return(<div>
+        <li>{output}</li>
+        <button onClick={()=>setDisplay(false)}>Change</button>
+    </div>);
+
+ 
 
 }
