d31a7fdce54cd94665ed1df3eba5c2dc03c9abe1
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-20 02:19:09 +0500

    28, - ,Rendering list, refactorign

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,10 @@
 export default function Products() {
 
     const products =["Dell", "HP", "Toshiba"];
+    const list = products.map((product)=>(<h4>{product}</h4>));
     return(
         <div>
-            {products.map((product)=>(
-                <li>{product}</li>
-            ))}
+            {list}
         </div>
     );
 
