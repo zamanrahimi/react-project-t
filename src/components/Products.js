4b971ab47f7892ee825ab8bb8b23616a77b5c3f3
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-20 12:30:30 +0500

    29, - ,render a key value list

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,18 @@
 export default function Products() {
 
-    const products =["Dell", "HP", "Toshiba"];
-    const list = products.map((product)=>(<h4>{product}</h4>));
+    //1. list
+    // const products =["Dell", "HP", "Toshiba"];
+    // 2. key value: dictionary 
+    const products =[
+        {id:1, name:'Dell', price:500},
+        {id:2, name:'HP', price:502},
+        {id:3, name:'Toshiba', price:503},
+    ];
+        const list =products.map((product)=>
+            <h3 key={product.id}>{product.name} - {product.price}</h3>
+        );
+
+
     return(
         <div>
             {list}
