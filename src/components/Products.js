f4cfd6ab8361978f4f2007bb9fa5e69f744f3143
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-20 12:45:03 +0500

    30, - ,Render an Array with its index as the key

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,16 +2,24 @@ export default function Products() {
 
     //1. list
     // const products =["Dell", "HP", "Toshiba"];
-    // 2. key value: dictionary 
-    const products =[
-        {id:1, name:'Dell', price:500},
-        {id:2, name:'HP', price:502},
-        {id:3, name:'Toshiba', price:503},
-    ];
-        const list =products.map((product)=>
-            <h3 key={product.id}>{product.name} - {product.price}</h3>
-        );
 
+    // 2. key value: dictionary 
+    // const products =[
+    //     {id:1, name:'Dell', price:500},
+    //     {id:2, name:'HP', price:502},
+    //     {id:3, name:'Toshiba', price:503},
+    // ];
+    //     const list =products.map((product)=>
+    //         <h3 key={product.id}>{product.name} - {product.price}</h3>
+    //     );
+
+    //3. Render array with its index
+
+    const products = ["Apple", "Banana", "Orange"];
+
+    const list = products.map((product, index)=>
+    <h3 key={index}>{index+1} - {product}</h3>
+    );
 
     return(
         <div>
